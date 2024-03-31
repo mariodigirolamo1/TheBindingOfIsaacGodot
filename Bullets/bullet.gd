80bbb153b601d88101746ff30304250357f60f9f
mariodigirolamo1 <mariodigirolamowork@gmail.com>
2024-03-31 12:44:59 +0200

    feat: add basic chasing enemies

--- Bullets/bullet.gd
+++ Bullets/bullet.gd
@@ -30,7 +30,5 @@ func updateBulletPosition(delta):
 		"down":
 			position.y += 1 * speed * delta
 
-func _on_body_entered(body):
-	if body.is_in_group("mobs"):
-		body.queue_free()
+func _on_area_shape_entered(area_rid, area, area_shape_index, local_shape_index):
 	queue_free()
--- Mobs/frog.gd
+++ Mobs/frog.gd
@@ -1,13 +1,24 @@
 extends CharacterBody2D
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+const SPEED = 30
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	var playerPosition = get_node("../../Player").position
+	var moveVector = playerPosition - position
+	
+	var x = moveVector.x
+	var y = moveVector.y
+	
+	var xDir = sign(x)
+	var yDir = sign(y)
+	
+	velocity.x = xDir * SPEED
+	velocity.y = yDir * SPEED
+	
+	move_and_slide()
 
 func _on_bullet_detection_area_shape_entered(area_rid, area, area_shape_index, local_shape_index):
 	if area.is_in_group("bullets"):
 		queue_free()
+	pass
