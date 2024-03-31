f7132c584c808df9dcf95e8640b306abb85e22a3
mariodigirolamo1 <mariodigirolamowork@gmail.com>
2024-03-31 16:23:40 +0200

    feat: basic healthbar UI
    modify frog collisions based on animatiom frame

--- Bullets/bullet.gd
+++ Bullets/bullet.gd
@@ -28,6 +28,3 @@ func updateBulletPosition(delta):
 			position.y += -1 * speed * delta
 		"down":
 			position.y += 1 * speed * delta
-
-func _on_area_shape_entered(area_rid, area, area_shape_index, local_shape_index):
-	queue_free()
