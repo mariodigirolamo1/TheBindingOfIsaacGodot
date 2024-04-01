509b50a05fb7f52750c4d0c31222edeb9033d750
mariodigirolamo1 <mariodigirolamowork@gmail.com>
2024-04-01 07:48:16 +0200

    feat: add main menu and also use as death screen

--- Global/player_stats.gd
+++ Global/player_stats.gd
@@ -6,8 +6,20 @@ var playerHP = 3
 var damageDelta = 1
 
 func _process(delta):
+	updateDamageDelta(delta)
+	
+	if playerHP == 0:
+		get_tree().change_scene_to_file("res://Menus/main.tscn")
+		resetPlayerStats()
+	
+func updateDamageDelta(delta):
 	if damageDelta < 1:
 		damageDelta += delta
+
+func resetPlayerStats():
+	playerHP = 3
+	maxPlayerHP = 3
+	damageDelta = 1
 
 func updatePlayerHP():
 	if damageDelta >= 1:
