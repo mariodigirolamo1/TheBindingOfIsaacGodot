348275787219a890081f3dc08d17017a647e4c88
mariodigirolamo1 <mariodigirolamowork@gmail.com>
2024-04-01 08:42:42 +0200

    feat: basic doors

---
+++ Doors/basic_door.gd
@@ -0,0 +1,14 @@
+extends Area2D
+
+var open = false
+
+func _process(delta):
+	if open:
+		get_node("Sprite").modulate = Color(0,0,255,0.5)
+	else:
+		get_node("Sprite").modulate = Color(255,0,0,0.5)
+
+func _on_body_entered(body):
+	if open:
+		if body.is_in_group("players"):
+			MapHandler.changeRoom()
--- Mobs/frog.gd
+++ Mobs/frog.gd
@@ -38,5 +38,6 @@ func _on_bullet_detection_area_shape_entered(area_rid, area, area_shape_index, l
 		playerInArea = true
 		
 func _on_detection_area_area_shape_exited(area_rid, area, area_shape_index, local_shape_index):
-	if area.is_in_group("players"):
-		playerInArea = false
+	if area != null:
+		if area.is_in_group("players"):
+			playerInArea = false
